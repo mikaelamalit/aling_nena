2f048cbf7b34d7de026fee169ad804a8c616c6cc
Mikaela T. Malit <ktm981@Mikaelas-MacBook-Air.local>
2014-05-11 18:30:10 +0800

    final version of project

--- main.rb
+++ main.rb
@@ -1,7 +1,59 @@
 require 'sinatra'
 require './boot.rb'
+require './money_calculator.rb'
+
+# ROUTES FOR CUSTOMER SECTION
+
+get '/' do
+  @show = Item.all
+  @products = @show.to_a.shuffle
+  erb :index
+end
+
+get '/about' do
+  erb :about
+end
+
+get '/contact' do
+  erb :contact
+end
+
+get '/products' do
+  @products = Item.all
+  erb :products
+end
+
+get '/purchase_form/:id' do
+  @product = Item.find(params[:id])
+  erb :purchase_form
+end
+
+post '/purchase/:id' do
+  @product = Item.find(params[:id])
+  @sold = params[:quantity]
+  @product.update_attributes!(
+    quantity: @product.quantity.to_i - @sold.to_i,
+    sold: @product.sold.to_i + @sold.to_i,
+  )
+  @total = @product.price.to_i * @sold.to_i
+  @payment = MoneyCalculator.new(
+    params[:ones].to_i, 
+    params[:fives].to_i,
+    params[:tens].to_i, 
+    params[:twenties].to_i, 
+    params[:fifties].to_i, 
+    params[:hundreds].to_i, 
+    params[:five_hundreds].to_i, 
+    params[:thousands].to_i,
+    @total.to_i
+    )
+  erb :purchase
+end
+
+# END OF ROUTES FOR CUSTOMER SECTION
 
 # ROUTES FOR ADMIN SECTION
+
 get '/admin' do
   @products = Item.all
   erb :admin_index
@@ -42,4 +94,4 @@ get '/delete_product/:id' do
   @product.destroy!
   redirect to '/admin'
 end
-# ROUTES FOR ADMIN SECTION
+# END ROUTES FOR ADMIN SECTION
--- money_calculator.rb
+++ money_calculator.rb
@@ -1,12 +1,29 @@
 class MoneyCalculator
+	attr_accessor :ones, :fives, :tens, :twenties, :fifties, :hundreds, :five_hundreds, :thousands, :total
 
-  def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands)
-    # each parameter represents the quantity per denomination of money
-    # these parameters can be assigned to instance variables and used for computation
-
-    # add a method called 'change' that takes in a parameter of how much an item costs
-    # and returns a hash of how much change is to be given
-    # the hash will use the denominations as keys and the amount per denomination as values
-    # i.e. {:fives => 1, fifties => 1, :hundreds => 3}
+	def initialize(ones, fives, tens, twenties, fifties, hundreds, five_hundreds, thousands, total)
+  	self.ones = ones
+  	self.fives = fives
+  	self.tens = tens
+  	self.twenties = twenties
+  	self.fifties = fifties
+  	self.hundreds = hundreds
+  	self.five_hundreds = five_hundreds
+  	self.thousands = thousands
+    self.total = total
   end
+
+  def change
+  	c_fives = self.fives.to_i * 5
+  	c_tens = self.tens.to_i * 10
+  	c_twenties = self.twenties.to_i * 20
+  	c_fifties = self.fifties.to_i * 50
+  	c_hundreds = self.hundreds.to_i * 100
+  	c_five_hundreds = self.five_hundreds.to_i * 500
+  	c_thousands = self.thousands.to_i * 1000
+  	payment = self.ones.to_i + c_fives + c_tens + c_twenties + c_fifties + c_hundreds + c_five_hundreds + c_thousands
+    change = payment.to_i - self.total.to_i
+    "P#{payment} ang ibinayad niyo.<br/><br/> P#{change} ang sukli niyo."
+  end
+
 end
